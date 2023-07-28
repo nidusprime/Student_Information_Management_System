ba2d1e2d9b90d764a9bcd9817644502253701da8
nidusprime <2962381324@qq.com>
2023-07-28 15:28:58 +0800

    Initial commit

---
+++ Stusystem.py
@@ -0,0 +1,17 @@
+"""
+简单的学生信息管理系统
+"""
+import os
+import re
+
+print('=' * 50)
+print('-' * 20 + 'Student Information Management System' + '-' * 20)
+print(' ' * 10 + "1.录入学生信息")
+print(' ' * 10 + "2.查找学生信息")
+print(' ' * 10 + "3.删除学生信息")
+print(' ' * 10 + "4.修改学生信息")
+print(' ' * 10 + "5.排序")
+print(' ' * 10 + "6.统计学生总人数")
+print(' ' * 10 + "7.显示所有学生信息")
+print(' ' * 10 + "0.退出系统")
+print('-' * 77)
