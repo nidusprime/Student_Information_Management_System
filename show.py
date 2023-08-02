8a17badc7e65f407fc5d3c3870194dfe8969ac4a
nidusprime <2962381324@qq.com>
2023-08-02 10:48:58 +0800

    Initial commit

--- show.py
+++ show.py
@@ -9,15 +9,21 @@ import search
 def show():
     if os.path.exists('stu_infor.txt'):
         search.show_gui()
+        stu_info_list = []
         with open('stu_infor.txt', 'r', encoding='utf-8') as file:
             stu_list = file.readlines()
         if len(stu_list) == 0:
             print('现在还没存储任何学生信息哦！')
         for item in stu_list:
-            ite = eval(item)
-            print(
-                '\t' + ite['id'] + '\t\t' + ite['name'] + '\t\t\t\t' + str(ite['math_grades']) + '\t\t\t\t' +
-                str(ite['chinese_grades']) + '\t\t\t\t\t' + str(ite['english_grades']) + '\t\t\t\t' +
-                str(ite['math_grades'] + ite['chinese_grades'] + ite['english_grades']))
+            stu_info_list.append(eval(item))
+        show_stu(stu_info_list)
     else:
         print('暂时没有查询到学生信息库！')
+
+
+def show_stu(list1):
+    for ite in list1:
+        print(
+            '\t' + ite['id'] + '\t\t' + ite['name'] + '\t\t\t\t' + str(ite['math_grades']) + '\t\t\t\t' +
+            str(ite['chinese_grades']) + '\t\t\t\t\t' + str(ite['english_grades']) + '\t\t\t\t' +
+            str(ite['math_grades'] + ite['chinese_grades'] + ite['english_grades']))
--- sort.py
+++ sort.py
@@ -1,7 +1,99 @@
 """
 根据某个信息对学生进行排序
 """
+import os.path
+
+import search
+import show
 
 
 def sort():
-    pass
+    if os.path.exists('stu_infor.txt'):
+        while True:
+            stu_lst = []
+            with open('stu_infor.txt', 'r', encoding='utf-8') as rfile:
+                stu_file = rfile.readlines()
+            for i in stu_file:
+                stu_lst.append(eval(i))
+            print('1.math_grades')
+            print('2.chinese_grades')
+            print('3.english_grades')
+            print('4.all_grades')
+            sel = input('请选择要根据哪门科目进行排序：')
+            sel_ud = input('请选择升序还是降序? 1·升序 2·降序:')
+            if sel_ud == '1':
+                if sel == '1' or sel == '2' or sel == '3' or sel == '4':
+                    if sel == '1':
+                        sort_list = up_sor(stu_lst, 'math_grades')
+                        search.show_gui()
+                        show.show_stu(sort_list)
+                        print("排序完成！")
+                    elif sel == '2':
+                        sort_list = up_sor(stu_lst, 'chinese_grades')
+                        search.show_gui()
+                        show.show_stu(sort_list)
+                        print("排序完成！")
+                    elif sel == '3':
+                        sort_list = up_sor(stu_lst, 'english_grades')
+                        search.show_gui()
+                        show.show_stu(sort_list)
+                        print("排序完成！")
+                    else:
+                        pass
+                else:
+                    print('输入1-4！输入有误')
+                    continue
+            else:
+                if sel == '1' or sel == '2' or sel == '3' or sel == '4':
+                    if sel == '1':
+                        sort_list = down_sor(stu_lst, 'math_grades')
+                        search.show_gui()
+                        show.show_stu(sort_list)
+                        print("排序完成！")
+                    elif sel == '2':
+                        sort_list = down_sor(stu_lst, 'chinese_grades')
+                        search.show_gui()
+                        show.show_stu(sort_list)
+                        print("排序完成！")
+                    elif sel == '3':
+                        sort_list = down_sor(stu_lst, 'english_grades')
+                        search.show_gui()
+                        show.show_stu(sort_list)
+                        print("排序完成！")
+                    else:
+                        pass
+                else:
+                    print('输入1-4！输入有误')
+                    continue
+            answer = input('是否要继续进行排序？y/n')
+            if answer == 'y' or answer == 'Y':
+                continue
+            else:
+                print('排序结束！')
+                break
+    else:
+        print('学生信息库暂时还未建立')
+
+
+def up_sor(list1, subjects):
+    for i in range(1, len(list1)):
+        for j in range(0, len(list1) - i):
+            if list1[j][subjects] <= list1[j + 1][subjects]:
+                continue
+            else:
+                list_va = list1[j]
+                list1[j] = list1[j + 1]
+                list1[j + 1] = list_va
+    return list1
+
+
+def down_sor(list1, subjects):
+    for i in range(1, len(list1)):
+        for j in range(0, len(list1) - i):
+            if list1[j][subjects] >= list1[j + 1][subjects]:
+                continue
+            else:
+                list_va = list1[j]
+                list1[j] = list1[j + 1]
+                list1[j + 1] = list_va
+    return list1
